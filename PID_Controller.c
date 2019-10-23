6508cd3e273e50549470aa2ffa634f9262616d91
hyh1121new <56910300+hyh1121new@users.noreply.github.com>
2019-10-23 16:05:10 +0800

    Create PID_Controller.c

---
+++ PID_Controller.c
@@ -0,0 +1,50 @@
+#include"PID_Controller.h"
+
+typedef struct
+    {
+      float SetPower;
+      float ActualPower;
+      float P_err;
+      float err_last;
+      float Kp;
+      float Ki;
+      float Kd;
+      float voltage;
+      float integral;
+    }PID_Parameter;
+    /**********************************
+    *
+    *
+    *
+    *
+    *
+    *
+    **********************************/
+    void Read_Parameter(void)
+    {
+      
+    }
+    /**********************************
+    *
+    *
+    *
+    *
+    *
+    *
+    **********************************/
+    void Set_Parameter(void)
+    {
+      
+    }
+    /**********************************
+    *
+    *
+    *
+    *
+    *
+    *
+    **********************************/
+    void PID_Controller(void)
+    {
+      
+    }
